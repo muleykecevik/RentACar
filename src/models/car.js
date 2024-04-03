8a3f69fd3de42c508ecc21b30f1f060c6b2ea804
muleykecevik <muleyke.cevik@gmail.com>
2024-04-03 20:54:17 +0300

    3/4/24

--- src/models/car.js
+++ src/models/car.js
@@ -5,64 +5,62 @@
 const { mongoose } = require('../configs/dbConnection')
 
 
-const CarSchema= new mongoose.Schema({
-    plateNumber:{
-        type:String,
-        trim:true,
-        unique:true,
-        required:true,
+const CarSchema = new mongoose.Schema({
+    plateNumber: {
+        type: String,
+        trim: true,
+        unique: true,
+        required: true,
     },
-    
-    brand:{
-        type:String,
-        trim:true,
-        required:true,
+
+    brand: {
+        type: String,
+        trim: true,
+        required: true,
 
     },
-    model:{
-        type:String,
-        trim:true,
-        required:true,
+    model: {
+        type: String,
+        trim: true,
+        required: true,
 
     },
     year: {
-        type:Number,
-        min:1950,
-        max:new Date().getFullYear(),
-        required:true,
+        type: Number,
+        min: 1950,
+        max: new Date().getFullYear(),
+        required: true,
     },
-    isAutomatic:{
-        type:Boolean,
+    isAutomatic: {
+        type: Boolean,
         default: false,
     },
-    pricePerDay:{
-        type:Number,
-        required:true,
+    pricePerDay: {
+        type: Number,
+        required: true,
     },
-    images:{
-        type:Array,
-        default:[]
+    images: {
+        type: Array,
+        default: []
     },
-    isAvailable:{
-        type:Boolean,
-        default:true,
+    isAvailable: {
+        type: Boolean,
+        default: true,
     },
-    createdId:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:'User',
-        required:true
+    createdId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
     },
-    updatedId:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:'User'
-        required:true
+    updatedId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
     },
-    
-    
-},{
-        collection: 'cars',
-        timestapms:true
-    })
+}, {
+    collection: 'cars',
+    timestapms: true
+})
 
 
-    module.exports= mongoose.model('Car',CarSchema)
+module.exports = mongoose.model('Car', CarSchema)
